2b93d866825ffd28012cb64b3cbc173a3bc8f2dd
tokebe <43009413+tokebe@users.noreply.github.com>
2021-12-01 13:49:32 -0500

    fix: comparison when one run had errors

--- controllers/compare.js
+++ controllers/compare.js
@@ -7,29 +7,46 @@ export default async function compare(newer, older) {
     if (older.summary.hasOwnProperty(query)) {
       const oldRes = older.summary[query];
       const newRes = newer.summary[query];
-      const itemsToCheck = [
-        oldRes.status === newRes.status,
-        oldRes.responseMB === newRes.responseMB,
-        oldRes.responseKB === newRes.responseKB,
-        oldRes.response.nodes === newRes.response.nodes,
-        oldRes.response.edges === newRes.response.edges,
-        oldRes.response.results === newRes.response.results,
-      ];
-      const changed = !itemsToCheck.every((check) => check);
-      const comparison = {
-        changed: changed,
-        status: `${oldRes.status} -> ${newRes.status}`,
-        responseTime: `${oldRes.responseTime} -> ${newRes.responseTime}`,
-        responseMB: `${oldRes.responseMB} -> ${newRes.responseMB}`,
-        responseKB: `${oldRes.responseKB} -> ${newRes.responseKB}`,
-        response: {
-          nodes: `${oldRes.nodes} -> ${newRes.nodes}`,
-          edges: `${oldRes.edges} -> ${newRes.edges}`,
-          results: `${oldRes.results} -> ${newRes.results}`,
-          olderLink: oldRes.response.link,
-          newerLink: newRes.response.link,
-        },
-      };
+      let comparison;
+      if (oldRes.hasOwnProperty('error') && !newRes.hasOwnProperty('error')) {
+        comparison = {
+          changed: true,
+          changes: "Old query failed with error",
+          error: oldRes.error,
+          newResults: { ...newRes },
+        }
+      } else if (!oldRes.hasOwnProperty('error') && newRes.hasOwnProperty('error')) {
+        comparison = {
+          changed: true,
+          changes: "New query failed with error",
+          error: newRes.error,
+          oldResults: { ...oldRes },
+        }
+      } else {
+        const itemsToCheck = [
+          oldRes.status === newRes.status,
+          oldRes.responseMB === newRes.responseMB,
+          oldRes.responseKB === newRes.responseKB,
+          oldRes.response.nodes === newRes.response.nodes,
+          oldRes.response.edges === newRes.response.edges,
+          oldRes.response.results === newRes.response.results,
+        ];
+        const changed = !itemsToCheck.every((check) => check);
+        comparison = {
+          changed: changed,
+          status: `${oldRes.status} -> ${newRes.status}`,
+          responseTime: `${oldRes.responseTime} -> ${newRes.responseTime}`,
+          responseMB: `${oldRes.responseMB} -> ${newRes.responseMB}`,
+          responseKB: `${oldRes.responseKB} -> ${newRes.responseKB}`,
+          response: {
+            nodes: `${oldRes.nodes} -> ${newRes.nodes}`,
+            edges: `${oldRes.edges} -> ${newRes.edges}`,
+            results: `${oldRes.results} -> ${newRes.results}`,
+            olderLink: oldRes.response.link,
+            newerLink: newRes.response.link,
+          },
+        };
+      }
       comparisonSummary[query] = comparison;
     } else {
       comparisonSummary[query] = {
